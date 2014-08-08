2ed9a9620979007610120e7ed65a481e78cb73dc
Chris Yeoh <cyeoh@au1.ibm.com>
2014-08-08 11:48:17 +0930

    Allow attaching external networks based on configurable policy
    Commit da66d50010d5b1ba1d7fc9c3d59d81b6c01bb0b0 restricted
    attaching external networks to admin clients. This patch changes
    it to a policy based check instead with the default setting being
    admin only. This allows operators to more precisely configure who
    they wish to allow to attach external networks without having to
    give them admin access
    
    Change-Id: I59e71f117f889f2abffddc36c1870ef1e0fe3711
    DocImpact: Adds network:attach_external_network policy
    Closes-Bug: #1352102
    (cherry picked from commit 674954f731bf4b66356fadaa5baaeb58279c5832)

--- nova/tests/network/test_neutronv2.py
+++ nova/tests/network/test_neutronv2.py
@@ -34,6 +34,8 @@ from nova.network.neutronv2 import api as neutronapi
 from nova.network.neutronv2 import constants
 from nova.objects import instance as instance_obj
 from nova.openstack.common import jsonutils
+from nova.openstack.common import policy as common_policy
+from nova import policy
 from nova import test
 from nova.tests import fake_instance
 from nova import utils
@@ -1726,6 +1728,13 @@ class TestNeutronv2(TestNeutronv2Base):
         req_ids = [net['id'] for net in self.nets5]
         self._get_available_networks(self.nets5, pub_nets=[],
                                      req_ids=req_ids, context=admin_ctx)
+
+    def test_get_available_networks_with_custom_policy(self):
+        rules = {'network:attach_external_network':
+                 common_policy.parse_rule('')}
+        policy.set_rules(rules)
+        req_ids = [net['id'] for net in self.nets5]
+        self._get_available_networks(self.nets5, pub_nets=[], req_ids=req_ids)
 
     def test_get_floating_ip_pools(self):
         api = neutronapi.API()
