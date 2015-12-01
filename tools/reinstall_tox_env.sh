a7f5340ff1ff249458b1bc02482fecb5a5cd4911
Jian Wen <wenjian@letv.com>
2015-12-01 10:26:26 +0800

    Make requirements more like our production env
    Change-Id: I0b3d433e1fcc388fa1e23cb7b38bb4851b83e772
    Closes-jira: OPENSTACK-430

--- tools/reinstall_tox_env.sh
+++ tools/reinstall_tox_env.sh
@@ -10,12 +10,10 @@ tox_version=$(tox --version| awk '{print $1}')
 
 for venv in pep8 py26
 do
-    sed -i "s|/root/zw/nova/.venv/bin/python|/usr/bin/python|" .tox/${venv}/.tox-config1
-    sed -i "s/1.8.1 0 0 1/${tox_version} 0 0 1/" .tox/${venv}/.tox-config1
-    sed -i "s/1.8.1 0 1 1/${tox_version} 0 0 1/" .tox/${venv}/.tox-config1
-    sed -i "s|/root/zw/nova|${TOP}|" .tox/${venv}/.tox-config1
-    sed -i "s|/root/zw/nova||" .tox/${venv}/lib/python2.6/site-packages/easy-install.pth
-    find .tox/${venv}/bin -type f -exec sed -i "s|/root/zw/nova|${TOP}|" {} \;
+    sed -i "s/1.6.0 0 0 1/${tox_version} 0 0 1/" .tox/${venv}/.tox-config1
+    sed -i "s|/root/nova|${TOP}|" .tox/${venv}/.tox-config1
+    sed -i "s|/root/nova||" .tox/${venv}/lib/python2.6/site-packages/easy-install.pth
+    find .tox/${venv}/bin -type f -exec sed -i "s|/root/nova|${TOP}|" {} \;
 
     source .tox/${venv}/bin/activate
     python setup.py develop --no-deps
